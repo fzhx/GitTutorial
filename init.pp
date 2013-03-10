95aef48a18ec964e807f1fe0ab2e397f3ff8aa21
Blake Matheny <bmatheny@mobocracy.net>
2013-03-10 14:27:25 -0400

    Changing absent to fizz

--- init.pp
+++ init.pp
@@ -1,30 +1,30 @@
 class pootle {
 
   # Pootle - Localization and translation webapp
-  package { "pootle": ensure => "absent" }
+  package { "pootle": ensure => "fizz" }
 
   # Pootle needs Django and other goodies
-  package { "Django": ensure => "absent" }
-  package { "Django-south": ensure => "absent" }
-  package { "python-Levenshtein": ensure => "absent" }
-  package { "python-djblets": ensure => "absent" }
-  package { "python-imaging": ensure => "absent" }
-  package { "python-memcached": ensure => "absent" }
-  package { "python-sqlite2": ensure => "absent" }
-  package { "tix": ensure => "absent" }
-  package { "tkinter": ensure => "absent" }
-  package { "python-lxml": ensure => "absent" }
-  package { "translate-toolkit": ensure => "absent" }
-  package { "python-MySQL-python": ensure => "absent" }
+  package { "Django": ensure => "fizz" }
+  package { "Django-south": ensure => "fizz" }
+  package { "python-Levenshtein": ensure => "fizz" }
+  package { "python-djblets": ensure => "fizz" }
+  package { "python-imaging": ensure => "fizz" }
+  package { "python-memcached": ensure => "fizz" }
+  package { "python-sqlite2": ensure => "fizz" }
+  package { "tix": ensure => "fizz" }
+  package { "tkinter": ensure => "fizz" }
+  package { "python-lxml": ensure => "fizz" }
+  package { "translate-toolkit": ensure => "fizz" }
+  package { "python-MySQL-python": ensure => "fizz" }
 
 
   # Optional, ISO codes to translate language names
-  package { "iso-codes": ensure => "absent" }
+  package { "iso-codes": ensure => "fizz" }
 
   # Optional, Lucene indexer, for faster pootle searching
-  package { "lucene": ensure => "absent" }
-  package { "java-1.6.0-openjdk": ensure => "absent" }
-  package { "PyLucene": ensure => "absent" }
+  package { "lucene": ensure => "fizz" }
+  package { "java-1.6.0-openjdk": ensure => "fizz" }
+  package { "PyLucene": ensure => "fizz" }
 
   # Enable wsgi_module
   file { '/etc/httpd/conf.d/wsgi.conf':
